46e8a146c370e6506fd9d4b21c71ea175a5f301b
kgaeul <gaury0722@naver.com>
2023-12-19 16:05:30 +0900

    ff

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Counter from './js/App';
 import reportWebVitals from './reportWebVitals';
+import ToggleButton from './js/ToggleButton';
+import IndexExample from './js/IndexExample';
+import ImageChange from './js/ImageChange';
+import ObjectExample from './js/ObjectExample';
+import AnimalSound from './js/Animal';
+import ExampleCallback from './js/ExampleCallback';
+import TimerCount from './js/TimerCount';
+import PracticeTwo from './js/PracticeTwo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   {/* <App />*/}
+    <Counter />
+    <ToggleButton />
+    <IndexExample />
+    <ImageChange />
+    <ObjectExample />
+    <AnimalSound />
+    <TimerCount />
+    <ExampleCallback />
+    <PracticeTwo />
   </React.StrictMode>
 );
 
@@ -15,3 +32,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+//<React.StrictMode> react 내부s에서 잠재적인 문제가 있는 지 알아내기 위한 도구
